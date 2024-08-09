ba16c22ce5192ba432baaec76899d5763dce6741
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2024-08-09 12:47:37 +0530

    transaction changes done

--- src/pages/admin/transaction.tsx
+++ src/pages/admin/transaction.tsx
@@ -62,7 +62,7 @@ const Transaction = () => {
     if (data)
       setRows(
         data.orders.map((i) => ({
-          user: i.user.name,
+          user: i.user ? i.user.name : "Unknown User", // Handle null or undefined user
           amount: i.total,
           discount: i.discount,
           quantity: i.orderItems.length,
